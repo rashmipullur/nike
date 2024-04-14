fb73a97370774341adcdbcf6d20ff9ffd4af9b4d
rashmipullur <mheresince1998@gmail.com>
2024-04-14 14:21:49 +0530

    nav + hero section + shoe card

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,12 @@ import {
   CustomerReviews,
   Footer,
 } from "./sections";
+import Nav from "./components/Nav";
 
 const App = () => (
   <main className="relative">
-    {/* <Nav /> */}
-    <section className="xl:padding-1 wide:padding-r padding-b">
+    <Nav />
+    <section className="xl:padding-l wide:padding-r padding-b">
       <Hero />
     </section>
     <section className="padding">
